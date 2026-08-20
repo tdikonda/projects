42fd3e05397117dee0b804e3c55650bb01165ac5
Tanuj Dikonda <tanujdikonda@gmail.com>
2026-08-20 13:53:15 -0400

    get 'nonDilutedMarketCap' key to fetch market cap

--- stock_tracker/stock_tracker.py
+++ stock_tracker/stock_tracker.py
@@ -75,7 +75,8 @@ class StockTracker:
                     market_cap = stock.info["marketCap"]
                 else:
                     current_price = round(stock.info["currentPrice"], 2)
-                    market_cap = stock.info["marketCap"]
+                    market_cap = stock.info["marketCap"] or stock.info[
+                        "nonDilutedMarketCap"]
 
                     # get Expected Future 1-Year Earnings Growth Rate
                     growth_estimates_df = stock.growth_estimates
